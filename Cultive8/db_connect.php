bbb5d5b6eea91338bf591b41989e326cd3ee3b37
ANTO ROSHAN A <antoroshan456@gmail.com>
2026-03-27 16:35:25 +0530

    Delete Cultive8 directory

--- Cultive8/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getDBConnection() {
-    $conn = mysqli_connect("localhost", "root", "", "cultive8_db");
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    return $conn;
-}
-?>
